1f170ecfd52a70bb29f4d926b17e3818da6c7b71
Byron Glover <byron.glover@newsignature.com>
2020-04-19 14:24:17 +0100

    Terraform format / validate

--- subscriptions/core/resource.keyvault.tf
+++ subscriptions/core/resource.keyvault.tf
@@ -8,7 +8,7 @@ data "azurerm_client_config" "current" {
 
 resource "azurerm_key_vault" "breakglass" {
   provider                    = azurerm.default
-  name                        = "${azurerm_resource_group.workload.name}-breakglass"
+  name                        = "${azurerm_resource_group.workload.name}breakglass"
   location                    = azurerm_resource_group.workload.location
   resource_group_name         = azurerm_resource_group.workload.name
   enabled_for_disk_encryption = true
